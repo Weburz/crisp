939f436f028a49297514557d375557cbd37c62df
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-07-13 01:45:27 +0530

    chore: add some more experimental code

--- cmd/message.go
+++ cmd/message.go
@@ -1,16 +1,27 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
 )
 
 var messageCmd = &cobra.Command{
-	Use:   "message",
-	Short: "The commit message to lint",
+	Use:     "message",
+	Short:   "The commit message to lint",
+	Example: "crisp message \"chore: fix an annoying bug\"",
+
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("requires at least one arg")
+		}
+
+		return fmt.Errorf("invalid colour specified: %s", args[0])
+	},
+
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello World!")
+		fmt.Println(args)
 	},
 }
 
