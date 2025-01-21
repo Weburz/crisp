1669c1d1c389578c3cad55477239d771236886e7
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-01-21 17:30:07 +0530

    chore: refactor the `version` command to be more apt

--- cmd/version.go
+++ cmd/version.go
@@ -1,16 +1,29 @@
 package cmd
 
 import (
-	"fmt"
+	"github.com/Weburz/crisp/internal/version"
 
 	"github.com/spf13/cobra"
 )
 
+var shortUsageHelp = "Print the version information."
+var longUsageHelp = `Prints a detailed version information of the application including
+version, commit hash, the build date and the version of Go used to develop it.`
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the Crisp version number.",
+	Short: shortUsageHelp,
+	Long:  longUsageHelp,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Crisp linter v0.0.1")
+		version := version.Get()
+		cmd.Println("### Crisp Build Information ###")
+		cmd.Printf("Version: \t%s\n", version.Version)
+		cmd.Printf("Git Version: \t%s\n", version.GitVersion)
+		cmd.Printf("Git Commit: \t%s\n", version.GitCommit)
+		cmd.Printf("Build Date: \t%s\n", version.BuildDate)
+		cmd.Printf("Go Version: \t%s\n", version.GoVersion)
+		cmd.Printf("Compiler: \t%s\n", version.Compiler)
+		cmd.Printf("Platform: \t%s\n", version.Platform)
 	},
 }
 
